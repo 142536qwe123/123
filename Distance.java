6e0e38b7abae44daaf0f968aeed0042a0283c030
142536qwe123 <10255102545@stu.ecnu.edu.cn>
2026-03-29 16:34:15 +0800

    Add files via upload

--- Distance.java
+++ Distance.java
@@ -1,32 +1,18 @@
-public class PrimeGenerator {
+public class Distance {
     public static void main(String[] args) {
-        int limit = 100;  // 默认值
-        
-        if (args.length > 0) {
-            limit = Integer.parseInt(args[0]);
+
+        if (args.length < 4) {
+            System.out.println("请提供 4 个参数：x1 y1 x2 y2");
+            return;
         }
-        
-        System.out.println("1到" + limit + "之间的素数：");
-        
-        for (int i = 2; i <= limit; i++) {
-            if (isPrime(i)) {
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println();  // 换行
-    }
-    
-    // 判断素数的函数（和 C 语言完全一样）
-    public static boolean isPrime(int n) {
-        if (n <= 1) return false;
-        if (n == 2) return true;
-        if (n % 2 == 0) return false;
-        
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
+
+        double x1 = Double.parseDouble(args[0]);
+        double y1 = Double.parseDouble(args[1]);
+        double x2 = Double.parseDouble(args[2]);
+        double y2 = Double.parseDouble(args[3]);
+
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println("两点之间的距离为：" + distance);
     }
 }
